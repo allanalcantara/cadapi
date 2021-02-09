e615b58441945f63aead6a10a3f9622a01c2a26f
Allan Alcântara <33518897+allanalcantara@users.noreply.github.com>
2021-02-08 21:30:48 -0300

    Create usuarios.ts

---
+++ app/models/usuarios.ts
@@ -0,0 +1,13 @@
+export interface Usuarios {
+
+    codigo: number;
+    nome: string;
+    sobrenome: string;
+    sexo: string;
+    dtnac: string;
+    email: string;
+    telefone: string;
+    rg: string;
+    cpf: string;
+
+}
